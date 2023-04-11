646fe7bac726c544199f98af68a383a962b4ef16
Henry Jiang <njjza@hotmail.com>
2023-04-11 15:54:34 -0700

    SparseMatrix

--- matrix/SparseMatrix.hpp
+++ matrix/SparseMatrix.hpp
@@ -1,40 +1,60 @@
-#include <iostream>
+#ifndef SPARSEMATRIX_H
+#define SPARSEMATRIX_H
 #include <map>
-
 class SparseMatrix {
 public:
     SparseMatrix() = default;
+    class Proxy {
+        public:
+            Proxy(SparseMatrix& parent, int key) : m_parent(parent), m_key(key) {}
 
-    double& operator[](int index) {
-        if (data.find(index) == data.end()) {
-            data[index] = 0;
-        }
-        return data[index];
+            operator double() const {
+                return m_parent.get_value(m_key);
+            }
+
+            Proxy& operator=(double value) {
+                m_parent.set_value(m_key, value);
+                return *this;
+            }
+
+        private:
+            SparseMatrix& m_parent;
+            int m_key;
+    };
+
+    Proxy operator[](int key) {
+        return Proxy(*this, key);
     }
 
-    const double& operator[](int index) const {
-        auto it = data.find(index);
-        if (it != data.end()) {
-            return it->second;
-        }
-        static const double defaultValue = 0;
-        return defaultValue;
+    double operator[](int key) const {
+        return get_value(key);
     }
 
-    SparseMatrix(const SparseMatrix& other) {
-        data = other.data;
+    bool has_key(int key) {
+        return m_map.find(key) != m_map.end();
     }
 
-    SparseMatrix& operator=(SparseMatrix other) {
-        swap(*this, other);
-        return *this;
-    }
-
-    friend void swap(SparseMatrix& first, SparseMatrix& second) {
-        using std::swap;
-        swap(first.data, second.data);
+    void swap(SparseMatrix& other) {
+        std::swap(m_map, other.m_map);
     }
 
 private:
-    std::map<int, double> data;
+    double get_value(int key) const {
+        auto it = m_map.find(key);
+        if (it != m_map.end()) {
+            return it->second;
+        }
+        return 0.0;
+    }
+
+    void set_value(int key, double value) {
+        if (value == 0.0) {
+            m_map.erase(key);
+        } else {
+            m_map[key] = value;
+        }
+    }
+
+    std::map<int, double> m_map;
 };
+#endif
